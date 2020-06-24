e180c2cfe89ea3a3e801ebeff8fa37aaf50a1710
Brandt Campbell <BCampbell@talentpath.com>
2020-06-23 21:47:37 -0400

    second commit

--- discgolf/src/main/java/com/bcampbell/discgolf/controllers/CourseController.java
+++ discgolf/src/main/java/com/bcampbell/discgolf/controllers/CourseController.java
@@ -3,6 +3,7 @@ package com.bcampbell.discgolf.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,26 +14,29 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bcampbell.discgolf.models.Course;
+import com.bcampbell.discgolf.repositories.CourseRepository;
 
 @RestController
 @RequestMapping("/api/v1/discgolf")
 public class CourseController {
 
+	@Autowired
+	private CourseRepository courseRepository;
+	
 	@GetMapping
 	public List<Course> list() {
-		List<Course> courses = new ArrayList<>();
-		return courses;
+		return courseRepository.findAll();
 	}
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.OK)
 	public void create(@RequestBody Course course) {
-		
+		courseRepository.save(course);
 	}
 	
 	@GetMapping("/{id}")
 	public Course get(@PathVariable("id") long id) {
-		return new Course();
+		return courseRepository.getOne(id);
 	}
 	
 }
--- discgolf/src/main/java/com/bcampbell/discgolf/models/Course.java
+++ discgolf/src/main/java/com/bcampbell/discgolf/models/Course.java
@@ -1,7 +1,20 @@
 package com.bcampbell.discgolf.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Course {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
 	private String name;
 	private String address;
 	private Integer holeNum;
@@ -30,6 +43,12 @@ public class Course {
 	}
 	public void setDifficulty(String difficulty) {
 		this.difficulty = difficulty;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
 	}
 	
 }
